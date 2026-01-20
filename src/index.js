878518be668477f75d08152afefca50b4d6c1fd6
Jazouli Mohamed <simojazouli2006@gmail.com>
2026-01-20 22:04:55 +0100

    import les pages

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Produits from './Produits'; 
 import './Produit.css';
+import  Formulaire from './Formulaire';
+import './Formulaire.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Produits />
+    <Formulaire />
   </React.StrictMode>
 );
 
